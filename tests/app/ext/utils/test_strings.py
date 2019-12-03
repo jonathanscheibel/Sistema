c7513acdafd033a38962388ca734c5ec3058301a
jonathan <jonathansmorais@gmail.com>
2019-12-02 23:07:18 -0300

    Mudança da chamada pytest

--- tests/app/ext/utils/test_strings.py
+++ tests/app/ext/utils/test_strings.py
@@ -2,4 +2,4 @@ from src.app.ext.utils.strings import get_random_string
 
 
 def test_get_random_string():
-    assert 'a' in get_random_string(24, 'dead')
+    assert 'd' in get_random_string(24, 'dead')
