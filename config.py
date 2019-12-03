a7adaac7d385d0c06f9d6c0a76396bc32c2cde8f
jonathan <jonathansmorais@gmail.com>
2019-12-02 23:19:42 -0300

    Refatoração de cast

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from decouple import config
 
 
 BASE_DIR = os.path.abspath('.')
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG')
 SECRET_KEY = config('SECRET_KEY') or ''.join(random.choice(string.ascii_letters) for i in range(42))
 SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI') or 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', True, cast=bool)
